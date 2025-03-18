79322a1e40ad473ba9964976bcd96d193837a2ef
slowy07 <slowy.arfy@proton.me>
2025-03-18 19:29:29 +0700

    chore: add avl trea
    add avl tree which is self-balancing binary search where the difference
    between heights of left and right substre for any node cannot be more
    than node
    
    reference:
    - https://en.wikipedia.org/wiki/AVL_tree
    
    Signed-off-by: slowy07 <slowy.arfy@proton.me>

--- README.md
+++ README.md
@@ -9,21 +9,34 @@ library for perform some complex data structure algorithms
 #include <iostream>
 
 int main() {
-  Graph<int> graph("undirected");
+    weight_graph<int> directed_graph("directed");
 
-  graph.add_edge(0, 1, 5);
-  graph.add_edge(0, 2, 3);
-  graph.add_edge(1, 2, 2);
-  graph.add_edge(1, 3, 4);
-  graph.add_edge(2, 3, 7);
+  std::cout << "test to add the directed graph" << std::endl;
+  directed_graph.add_edge(1, 2, 10);
+  directed_graph.add_edge(2, 3, 20);
+  directed_graph.add_edge(3, 4, 30);
+  directed_graph.add_edge(1, 3, 50);
 
-  std::cout << "DFS traversal (from 0): ";
-  std::vector<int> dfs_result = graph.dfs(0);
+  std::cout << "DFS traversal (directed graph): ";
+  std::vector<int> dfs_result = directed_graph.dfs(1);
   for (int node : dfs_result) {
     std::cout << node << " ";
   }
   std::cout << std::endl;
 
-  return 
+  weight_graph<int> undirectedGraph("undirected");
+  std::cout << "add edges to the undirected graph" << std::endl;
+  undirectedGraph.add_edge(1, 2, 10);
+  undirectedGraph.add_edge(2, 3, 20);
+  undirectedGraph.add_edge(3, 4, 30);
+  undirectedGraph.add_edge(1, 3, 50);
+  std::cout << "DFS traversal (undirected graph): ";
+  dfs_result = undirectedGraph.dfs(1);
+  for (int node : dfs_result) {
+    std::cout << node << " ";
+  }
+  std::cout << std::endl;
+
+  return 0;
 }
 ```
---
+++ example/avl_tree.cc
@@ -0,0 +1,54 @@
+#include "../enola/tree/avl_tree.hpp"
+#include <iostream>
+
+int main() {
+  avl_tree<int> tree;
+
+  std::cout << "Inserting elements: 10, 20, 30, 40, 50\n";
+  tree.insert(10);
+  tree.insert(20);
+  tree.insert(30);
+  tree.insert(40);
+  tree.insert(50);
+
+  std::cout << "inorder traversal: ";
+  std::vector<int> inorder_result = tree.inorder();
+  for (int value : inorder_result) {
+    std::cout << value << " ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "preorder traversal: ";
+  std::vector<int> preorder_result = tree.preorder();
+  for (int value : preorder_result) {
+    std::cout << value << " ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "postorder traversal: ";
+  std::vector<int> postorder_result = tree.postorder();
+  for (int value : postorder_result) {
+    std::cout << value << " ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "remove element 20" << std::endl;
+  tree.remove(20);
+
+  std::cout << "inorder traversal after removeing 20: ";
+  inorder_result = tree.inorder();
+  for (int value : inorder_result) {
+    std::cout << value << " ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "attempting remove non-existent element 60" << std::endl;
+  tree.remove(60);
+
+  std::cout << "inorder traversal after attempting to remove 60: ";
+  inorder_result = tree.inorder();
+  for (int value : inorder_result) {
+    std::cout << value << " ";
+  }
+  std::cout << std::endl;
+}
